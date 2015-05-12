2662638a4ca3cafed11ae310bf059c925b4e4efb
Rock <jennyliang.cal@gmail.com>
2015-05-11 23:25:22 -0700

    Update Code.R

--- Code.R
+++ Code.R
@@ -86,3 +86,102 @@ for (i in 1:5){
 }
 accuracy
 
+
+# Read in dataset
+quality = read.csv("quality.csv")
+
+# Look at structure
+str(quality)
+
+# Table outcome
+table(quality$PoorCare)
+
+# Baseline accuracy
+980/1310
+
+# Install and load caTools package
+#install.packages("caTools")
+library(caTools)
+
+# Randomly split data
+set.seed(88)
+split = sample.split(quality$PoorCare, SplitRatio = 0.75)
+split
+
+# Create training and testing sets
+qualityTrain = subset(quality, split == TRUE)
+qualityTest = subset(quality, split == FALSE)
+
+# Logistic Regression Model
+QualityLog = glm(PoorCare ~ OfficeVisits + Narcotics, data=qualityTrain, family=binomial)
+summary(QualityLog)
+
+# Make predictions on training set
+predictTrain = predict(QualityLog, type="response")
+
+# Analyze predictions
+summary(predictTrain)
+tapply(predictTrain, qualityTrain$PoorCare, mean)
+
+
+
+
+
+# Confusion matrix for threshold of 0.5
+table(qualityTrain$PoorCare, predictTrain > 0.5)
+
+# Sensitivity and specificity
+100/250
+700/740
+
+# Confusion matrix for threshold of 0.7
+table(qualityTrain$PoorCare, predictTrain > 0.7)
+
+# Sensitivity and specificity
+80/250
+730/740
+
+# Confusion matrix for threshold of 0.2
+table(qualityTrain$PoorCare, predictTrain > 0.2)
+
+# Sensitivity and specificity
+160/250
+540/740
+
+
+
+
+# Install and load ROCR package
+# install.packages("ROCR")
+library(ROCR)
+
+# Prediction function
+ROCRpred = prediction(predictTrain, qualityTrain$PoorCare)
+
+# Performance function
+ROCRperf = performance(ROCRpred, "tpr", "fpr")
+
+# Plot ROC curve
+plot(ROCRperf)
+
+# Add colors
+plot(ROCRperf, colorize=TRUE)
+
+# Add threshold labels 
+plot(ROCRperf, colorize=TRUE, print.cutoffs.at=seq(0,1,by=0.1), text.adj=c(-0.2,1.7))
+
+#Compute the test set prediction
+
+predictTest = predict(QualityLog, type="response", newdata=qualityTest)
+
+# compute the test set AUC
+  
+ROCRpredTest = prediction(predictTest, qualityTest$PoorCare)
+
+auc = as.numeric(performance(ROCRpredTest, "auc")@y.values)
+auc
+
+a=table(qualityTest$PoorCare, predictTest > 0.2)
+accuracy=sum(diag(a))/sum(a)
+accuracy
+
